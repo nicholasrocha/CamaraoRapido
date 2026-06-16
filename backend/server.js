9ac056b42e6511db2ab2b8472fc13519440b812c
Nicholas Rocha <rochanicholas10@gmail.com>
2026-06-15 22:49:29 -0300

    Ajuste para deploy Render

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,11 @@
-const express = require('express');
-
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
-const path = require("path");
 
 app.use(
   express.static(
@@ -16,18 +13,27 @@ app.use(
   )
 );
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 app.use(routes);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-  console.log("Servidor rodando em http://localhost:3000");
+app.listen(PORT, () => {
+
+  console.log(
+    `Servidor rodando na porta ${PORT}`
+  );
 
 });
 
-const db = require('./db');
+const db = require("./db");
 
-db.query('SELECT NOW()')
+db.query("SELECT NOW()")
   .then(() => console.log("Banco conectado!"))
-  .catch(err => console.error("Erro no banco:", err));
+  .catch(err =>
+    console.error(
+      "Erro no banco:",
+      err
+    )
+  );
